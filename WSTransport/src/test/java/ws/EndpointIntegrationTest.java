c8173a3f6cd2fdc41683dbfbfb69689df6007570
IljaKroonen <ilja.kroonen@gmail.com>
2014-03-29 16:02:21 +0100

    Set up of integration test ; test does not pass because the framework somehow doesnt find the endpoint

--- WSTransport/src/test/java/ws/EndpointIntegrationTest.java
+++ WSTransport/src/test/java/ws/EndpointIntegrationTest.java
@@ -1,18 +1,23 @@
 package ws;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ws.test.server.MockWebServiceClient;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import static org.springframework.ws.test.server.RequestCreators.withPayload;
 import static org.springframework.ws.test.server.ResponseMatchers.payload;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("application-context.xml")
 public class EndpointIntegrationTest {
 	@Autowired
 	private ApplicationContext applicationContext;
